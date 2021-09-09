bd6325d658847a7a4de78774d59b7f33fecb22ec
rony <rriachi@bright-lab.com>
2021-09-09 16:50:40 +0300

    profile with text

--- components/Alexisran.js
+++ components/Alexisran.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 function Alexisran(props) {
     return (
-        <div className = "pt-20 border-b-2">
+        <div className = "pt-20 border-b-2 container mx-auto pb-20">
             <div className = "container mx-auto">
                 <div className = "grid grid-cols-2">
                     <div className = "col-span-1">
@@ -12,8 +12,8 @@ function Alexisran(props) {
                     </div>
 
                     <div>
-                    <div className = "font-bold">{props.name}.</div>
-                    <div className = "text-gray-500 pt-5">{props.position}</div>
+                    <div className = "font-bold">{props.name} {props.name2}</div>
+                    <div className = "text-gray-500 pt-5 text-sm">{props.position} {props.position2}</div>
 
                     </div>
                 </div>
