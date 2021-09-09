163d1a503479e19651915b07a24ccfbd2d0b75c2
rony <rriachi@bright-lab.com>
2021-09-09 15:10:39 +0300

    small box

---
+++ components/Smallbox.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function Smallbox() {
+    return (
+        <div className = "bg-white absolute bottom-0 left-0 ">
+        <div className = "bg-white container mx-auto">
+        <p className = "font-bold">DEVELOPING AN APPLICATION IS ALL ABOUT GIVING SOLUTION FOR A SPECIFIC PROBLEM</p>
+        <p className = "text-gray-500 inline">MARCH 15, 2016</p>
+        <p className = "inline ml-4 bg-blue-400 text-white font-bold p-1 ">Life / Travel</p>
+      </div>
+        </div>
+    )
+}
+
+export default Smallbox
