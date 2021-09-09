56621d120bd54a2ee1cb76f08b0297c078d2a2c4
rony <rriachi@bright-lab.com>
2021-09-09 15:10:03 +0300

    left image with text

---
+++ components/Textleftpic.js
@@ -0,0 +1,26 @@
+import React from 'react'
+
+function Textleftpic(props) {
+    return (
+        <div>
+            <div className = "container mx-auto">
+                <div>
+                    <p>{props.title}</p>
+                </div>
+                <div>
+                    <div className = "grid grid-cols-2">
+                        <div className = "image"></div>
+                        <div>
+                        <div>{props.title}</div>
+                        <div className = "pt-6 pb-20">{props.title}</div>
+                        </div>
+                        
+                        
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Textleftpic
