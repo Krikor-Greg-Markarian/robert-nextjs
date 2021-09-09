3dba95a90a41c6c26d4008fbc4675c2a0fd0ffee
rony <rriachi@bright-lab.com>
2021-09-09 12:36:51 +0300

    update

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,36 @@
 import Head from 'next/head'
 import Image from 'next/image'
+import Blacknavbar from '../components/Blacknavbar'
 import styles from '../styles/Home.module.css'
+import { FaSearch, FaTwitter, FaEnvelopeOpenText,FaPhoneAlt,FaCommentDots } from 'react-icons/fa';
+import Bluenavbar from '../components/Bluenavbar';
+import Backgroundimage from '../components/Backgroundimage';
 
 export default function Home() {
   return (
     <div>
-      <div className = "font-bold">asdsadasd</div>
+
+
+
+
+
+      <section>
+      <Blacknavbar />
+      </section>
+
+
+      <section>
+        <Bluenavbar />
+       
+      </section>
+
+
+    <section>
+      <Backgroundimage />
+    </section>
+
+
+
     </div>
   )
 }
