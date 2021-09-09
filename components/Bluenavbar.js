449c71e9295490abedf25f7703ac8550ffa58875
rony <rriachi@bright-lab.com>
2021-09-09 15:09:48 +0300

    blue navbar

--- components/Bluenavbar.js
+++ components/Bluenavbar.js
@@ -4,36 +4,31 @@ import { FaSearch,FaCommentDots } from "react-icons/fa";
 function Bluenavbar() {
   return (
     <div>
-      <div className = "bg-blue-500">
-
-
-        <div className = "container mx-auto">
-            <div className = "pt-3 pb-3 inline">
-            
-            
-            
-            <div className = "">
-            <ul className = "">
-                <li className = "text-white inline ml-3"><a href="#">Home</a></li>
-                <li className = "text-white inline ml-3"><a href="#">About me </a></li>
-                <li className = "text-white inline ml-3"><a href="#">Skills</a></li>
-                <li className = "text-white inline ml-3"><a href="#">Resource</a></li>
-                <li className = "text-white inline ml-3"><a href="#">Services</a></li>
-                <li className = "text-white inline ml-3"><a href="#">Portfolio</a></li>
-                <li className = "text-white inline ml-3"><a href="#">Team</a></li>
-                <li className = "text-white inline ml-3"><a href="#">Hire me</a></li>
-            </ul>
-            </div>
-            </div>
-
-            <div>
-                <FaSearch className = "text-white inline"/>
-                <FaCommentDots className = "text-white inline ml-3"/>
-            </div>
-        </div>
-
-
-
+      <div className = "bg-blue-500 ">
+          <div className = "container mx-auto">
+              <div className = "">
+                <div className = "flex justify-start items-center">
+                <p className = "italic font-bold text-white inline">Robert</p>
+                </div>
+                <ul className = "inline-block flex justify-center items-center">
+                  <li className = "text-white inline ml-3"><a href="#">Home</a></li>
+                  <li className = "text-white ml-3 inline"><a href="#">About me</a></li>
+                  <li className = "text-white ml-3 inline"><a href="#">Skills</a></li>
+                  <li className = "text-white ml-3 inline"><a href="#">Resume</a></li>
+                  <li className = "text-white ml-3 inline"><a href="#">Service</a></li>
+                  <li className = "text-white ml-3 inline"><a href="#">Portfolio</a></li>
+                  <li className = "text-white ml-3 inline"><a href="#">Team</a></li>
+                  <li className = "text-white ml-3 inline"><a href="#">Hire me</a></li>
+                  
+                <div className = "flex justify-end items-end ">
+                <FaSearch className = "inline text-white "/>
+                <FaCommentDots className = "inline text-white"/>
+                </div>
+                
+                </ul>
+               
+              </div>
+          </div>
       </div>
     </div>
   );
