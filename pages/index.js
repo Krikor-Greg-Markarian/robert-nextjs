e1cb85e310197cb1f55482d9871e3dbf6de4a0fe
rony <rriachi@bright-lab.com>
2021-09-09 16:50:45 +0300

    update

--- pages/index.js
+++ pages/index.js
@@ -5,6 +5,8 @@ import Smallbox from "../components/Smallbox";
 import Textleftpic from "../components/Textleftpic";
 import { FaFacebook, FaInstagram, FaTwitter,FaMailBulk,FaMapMarker } from "react-icons/fa";
 import Alexisran from "../components/Alexisran";
+import Alexisran2 from "../components/Alexisran";
+import Relatedposts from "../components/Relatedposts";
 
 const object = {
   data: [
@@ -13,8 +15,14 @@ const object = {
         "Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas nemo repellendus, quasi tenetur ipsam debitis tempora, numquam saepe autem, veniam vero sint? Quaerat accusantium iure quibusdam veniam voluptatem modi quod.",
 
         name: "Alexis Ran",
-        position: "Many desktop publishing and web page editors now use lorem ipsum as their default model text, andaw search to lorem ipsum will uncover many web sites stilin. Their infancy. Various versions have envolved over."
+        position: "Many desktop publishing and web page editors now use lorem ipsum as their default model text, andaw search to lorem ipsum will uncover many web sites stilin. Their infancy. Various versions have envolved over.",
+
+        name2: "Alexis Ran",
+        position2: "Proin gravida nibh vel velit auctor aliquet Aenean sollicitudin. Lorem quisma bibendum ctor, nisi. elitas consequat ipsum nec sagittis sem nibh id elit. Duis sed odios sit amet nibh vulputate cursus a sit amet mauris. Morbi accumsan ipsum velit. Nam nec tellus a odio ticidut."
+
+        
     },
+    
   ],
 };
 
@@ -69,6 +77,36 @@ export default function Home() {
           <Alexisran key={idx} name={item.name} position={item.position}/>
         ))}
        
+      </section>
+
+
+      <section >
+        <div className = "pt-40 pb-20">
+          <div className = "container mx-auto">
+            <div className = "flex justify-center items-center">
+              <p className = "font-bold border-b-4 text-sm">RELATED POSTS</p>
+              
+              </div>
+              <div className = "grid grid-cols-2 gap-40">
+                <div className = "col-span-1">
+              <Relatedposts />
+             
+              </div>
+              <Relatedposts />
+              </div>
+              <div className = "flex justify-center items-center pt-20">
+              <p className = "font-bold border-b-4 text-sm">COMMENTS</p>
+              
+              </div>
+              {object.data.map((item, idx) => (
+          <Alexisran2 key={idx} name={item.name2} position={item.position2}/>
+        ))}
+          </div>
+     
+        </div>
+
+        
+        
       </section>
     </div>
   );
