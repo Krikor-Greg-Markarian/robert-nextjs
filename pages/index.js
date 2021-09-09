bd59c01ca935f49e6dd4fc796bf84d4d898d5d90
rony <rriachi@bright-lab.com>
2021-09-09 15:10:09 +0300

    update

--- pages/index.js
+++ pages/index.js
@@ -1,36 +1,59 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import Blacknavbar from '../components/Blacknavbar'
-import styles from '../styles/Home.module.css'
-import { FaSearch, FaTwitter, FaEnvelopeOpenText,FaPhoneAlt,FaCommentDots } from 'react-icons/fa';
-import Bluenavbar from '../components/Bluenavbar';
-import Backgroundimage from '../components/Backgroundimage';
+import Blacknavbar from "../components/Blacknavbar";
+import Bluenavbar from "../components/Bluenavbar";
+import Backgroundimage from "../components/Backgroundimage";
+import Smallbox from "../components/Smallbox";
+import Textleftpic from "../components/Textleftpic";
+import { FaFacebook, FaInstagram, FaTwitter,FaMailBulk,FaMapMarker } from "react-icons/fa";
+
+const object = {
+  data: [
+    {
+      title:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas nemo repellendus, quasi tenetur ipsam debitis tempora, numquam saepe autem, veniam vero sint? Quaerat accusantium iure quibusdam veniam voluptatem modi quod.",
+    },
+  ],
+};
+
+
 
 export default function Home() {
   return (
     <div>
-
-
-
-
-
       <section>
-      <Blacknavbar />
+        <Blacknavbar />
       </section>
-
 
       <section>
         <Bluenavbar />
-       
       </section>
 
+      <section className="">
+        <Backgroundimage>
+          <Smallbox />
+        </Backgroundimage>
+      </section>
 
-    <section>
-      <Backgroundimage />
-    </section>
-
-
-
+      <section className = "pt-8">
+        {object.data.map((item, idx) => (
+          <Textleftpic key={idx} title={item.title} />
+        ))}
+        
+        <div  className = "container mx-auto border-2 p-4 ">
+          <div>
+      <p className = "font-bold inline border-r-2 pl-4 pr-4">2 COMMENTS </p>
+      <p className = "font-bold inline ml-1 pl-4 pr-4 ">  TAGGED :</p> 
+      <span className = "text-gray-500 inline">LIFESTYLE, FOOD, DRINK</span> 
+      <div className = "float-right">
+        <FaFacebook  className = "inline"/>
+        <FaTwitter className = "inline ml-3"/>
+        <FaMailBulk className = "inline ml-3"/>
+        <FaInstagram className = "inline ml-3"/>
+        <FaMapMarker className = "inline ml-3"/>
+      </div>
+      </div>
+     
+      </div>
+      </section>
     </div>
-  )
+  );
 }
