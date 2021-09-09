38e906d92bd272a8679f32c0f8bdf61a7408aed2
rony <rriachi@bright-lab.com>
2021-09-09 18:03:01 +0300

    text and images

--- components/Alexisran.js
+++ components/Alexisran.js
@@ -1,25 +1,22 @@
-import React from 'react'
+import React from "react";
 
 function Alexisran(props) {
-    return (
-        <div className = "pt-20 border-b-2 container mx-auto pb-20">
-            <div className = "container mx-auto">
-                <div className = "grid grid-cols-2">
-                    <div className = "col-span-1">
-                        <div className = "alexisran rounded-full "></div>
-                        
-                       
-                    </div>
+  return (
+    <div className="pt-20 border-b-2 container mx-auto pb-20">
+      <div className="container mx-auto">
+        <div className="grid grid-cols-2">
+          <div className="col-span-1">
+            <div className="alexisran rounded-full "></div>
+          </div>
 
-                    <div>
-                    <div className = "font-bold">{props.name} {props.name2}</div>
-                    <div className = "text-gray-500 pt-5 text-sm">{props.position} {props.position2}</div>
-
-                    </div>
-                </div>
-            </div>
+          <div>
+            <div className="font-bold">{props.name}</div>
+            <div className="text-gray-500 pt-5 text-sm">{props.position}</div>
+          </div>
         </div>
-    )
+      </div>
+    </div>
+  );
 }
 
-export default Alexisran
+export default Alexisran;
