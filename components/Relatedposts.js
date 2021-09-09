2e56f0b775d18c40652be9d48b3f35aeb35cbf8c
rony <rriachi@bright-lab.com>
2021-09-09 16:51:30 +0300

    background pic with text

---
+++ components/Relatedposts.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+function Relatedposts() {
+  return (
+    <div>
+      <div className="pt-12">
+        
+            <div className="relatedposts relative">
+              <div className="absolute -bottom-3 -right-3">
+                <div className="bg-white w-40 p-2 h-22 font-bold">
+                  Tips for developing a high-end Mobile App that
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+     
+  );
+}
+
+export default Relatedposts;
