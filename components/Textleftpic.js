d915a09e0bcafce52636de89a7c0681079d177ad
rony <rriachi@bright-lab.com>
2021-09-10 10:29:34 +0300

    small box with text

--- components/Textleftpic.js
+++ components/Textleftpic.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 function Textleftpic(props) {
     return (
-        <div>
+        <div className = "pt-20">
             <div className = "container mx-auto">
                 <div>
                     <p>{props.title}</p>
