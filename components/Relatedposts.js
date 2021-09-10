e00fb23ec12369b2be02f2fc1488bc76f5b326da
rony <rriachi@bright-lab.com>
2021-09-10 11:56:36 +0300

    box with absolute box

--- components/Relatedposts.js
+++ components/Relatedposts.js
@@ -1,14 +1,18 @@
 import React from "react";
 
-function Relatedposts() {
+function Relatedposts(props) {
   return (
     <div>
       <div className="pt-12">
         
             <div className="relatedposts relative">
               <div className="absolute -bottom-3 -right-3">
-                <div className="bg-white w-40 p-2 h-22 font-bold">
-                  Tips for developing a high-end Mobile App that
+                
+                <div className="bg-white w-60 p-2 h-32 font-bold">
+                  <div className = "flex justify-end items-center">
+                    <button className = "bg-blue-500 p-1 text-white ">{props.music}{props.musictwo}</button>
+                  </div>
+                  {props.something}{props.somethingtwo}
                 </div>
               </div>
             </div>
--- components/Smallbox.js
+++ components/Smallbox.js
@@ -2,11 +2,14 @@ import React from 'react'
 
 function Smallbox() {
     return (
-        <div className = "bg-white absolute bottom-0 left-0 ">
+        <div className = "bg-white absolute -bottom-16 left-60 pt-6 w-1/2 ">
         <div className = "bg-white container mx-auto">
-        <p className = "font-bold">DEVELOPING AN APPLICATION IS ALL ABOUT GIVING SOLUTION FOR A SPECIFIC PROBLEM</p>
-        <p className = "text-gray-500 inline">MARCH 15, 2016</p>
-        <p className = "inline ml-4 bg-blue-400 text-white font-bold p-1 ">Life / Travel</p>
+        <p className = "font-bold text-center">DEVELOPING AN APPLICATION IS ALL ABOUT GIVING SOLUTION FOR A SPECIFIC PROBLEM</p>
+        <div>
+         <div className = "pt-8"></div>
+        </div>
+        <p className = "text-gray-500 inline flex justify-center items-center">MARCH 15, 2016 <div className = "ml-2"><button className = "bg-blue-400 p-1 text-white px-4">Life / Travel</button></div> </p>
+        
       </div>
         </div>
     )
