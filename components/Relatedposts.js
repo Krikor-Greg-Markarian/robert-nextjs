e2c89a0a52c7f59940526a9d8ccc790fe05a3bd6
rony <rriachi@bright-lab.com>
2021-09-10 14:28:04 +0300

    background img with absolute image

--- components/Relatedposts.js
+++ components/Relatedposts.js
@@ -8,11 +8,11 @@ function Relatedposts(props) {
             <div className="relatedposts relative">
               <div className="absolute -bottom-3 -right-3">
                 
-                <div className="bg-white w-60 p-2 h-32 font-bold">
+                <div className="bg-white w-60 h-32 font-bold p-4">
                   <div className = "flex justify-end items-center pt-2">
                     <button className = "bg-blue-500 p-1 text-white ">{props.music}{props.musictwo}</button>
                   </div>
-                  <div className = "pt-2">
+                  <div className = "pt-1">
                   {props.something}{props.somethingtwo}
                   </div>
                 </div>
