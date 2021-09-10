4725ca84b1f6cdeb4ff9d8777cc1c593b4d21751
rony <rriachi@bright-lab.com>
2021-09-10 11:56:45 +0300

    left picture with text

--- components/Textleftpic.js
+++ components/Textleftpic.js
@@ -5,14 +5,16 @@ function Textleftpic(props) {
         <div className = "pt-20">
             <div className = "container mx-auto">
                 <div>
-                    <p>{props.title}</p>
+                    <p className = "text-gray-500 pb-4">{props.title}</p>
                 </div>
                 <div>
                     <div className = "grid grid-cols-2">
+                        <div className = "pt-2 pr-12">
                         <div className = "image"></div>
+                        </div>
                         <div>
-                        <div>{props.title}</div>
-                        <div className = "pt-6 pb-20">{props.title}</div>
+                        <div className = "text-gray-500 ">{props.title}</div>
+                        <div className = "pt-6 pb-20 text-gray-500">{props.title}</div>
                         </div>
                         
                         
