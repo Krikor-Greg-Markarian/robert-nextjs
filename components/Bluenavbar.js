3d08f1396b893d44c028333bb5ba6c62af9c41e4
rony <rriachi@bright-lab.com>
2021-09-10 15:38:14 +0300

    blue navbar

--- components/Bluenavbar.js
+++ components/Bluenavbar.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { FaSearch,FaCommentDots } from "react-icons/fa";
+import { FaSearch, FaCommentDots } from "react-icons/fa";
 
 function Bluenavbar() {
   return (
@@ -30,33 +30,46 @@ function Bluenavbar() {
               </div>
           </div>
       </div> */}
-      <div className = "bg-blue-500 p-4">
-      <div className = "container mx-auto">
-        <div className = "flex justify-start items-center">
-        <div>
-          <p className = "text-white italic">Robert</p>
-        </div>
-        <div className = "pl-8">
-          <ul>
-            <li className = "text-white inline ml-12"><a href="#">Home</a></li>
-            <li className = "text-white inline ml-5"><a href="#">About me</a></li>
-            <li className = "text-white inline ml-5"><a href="#">Skills</a></li>
-            <li className = "text-white inline ml-5"><a href="#">Resume</a></li>
-            <li className = "text-white inline ml-5"><a href="#">Services</a></li>
-            <li className = "text-white inline ml-5"><a href="#">Portfolio</a></li>
-            <li className = "text-white inline ml-5"><a href="#">Team</a></li>
-            <li className = "text-white inline ml-5"><a href="#">Hire me</a></li>
-            
-          </ul>
-        </div>
-        <div className = "text-white pl-12">
-          <div className = "">
-            <FaSearch className = "inline "/>
-            <FaCommentDots className = "inline ml-2"/>
+      <div className="bg-blue-500 p-4 ">
+        <div className="container mx-auto ">
+          <div className="flex justify-start items-center">
+            <p className="text-white italic">Robert</p>
+
+            <div className="pl-8">
+              <ul>
+                <li className="text-white inline ml-12">
+                  <a href="#">Home</a>
+                </li>
+                <li className="text-white inline ml-5">
+                  <a href="#">About me</a>
+                </li>
+                <li className="text-white inline ml-5">
+                  <a href="#">Skills</a>
+                </li>
+                <li className="text-white inline ml-5">
+                  <a href="#">Resume</a>
+                </li>
+                <li className="text-white inline ml-5">
+                  <a href="#">Services</a>
+                </li>
+                <li className="text-white inline ml-5">
+                  <a href="#">Portfolio</a>
+                </li>
+                <li className="text-white inline ml-5">
+                  <a href="#">Team</a>
+                </li>
+                <li className="text-white inline ml-5">
+                  <a href="#">Hire me</a>
+                </li>
+              </ul>
+            </div>
+            <div className="text-white ml-3">
+              <div className="ml-2">
+                <FaSearch className="inline ml-2" />
+                <FaCommentDots className="inline ml-6" />
+              </div>
+            </div>
           </div>
-          
-        </div>
-        </div>
         </div>
       </div>
     </div>
