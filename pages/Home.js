d75901711df1be5d351477ab2e1d898d37ff2dfe
rony <rriachi@bright-lab.com>
2021-09-10 17:31:36 +0300

    second project

--- pages/Home.js
+++ pages/Home.js
@@ -3,6 +3,20 @@ import Link from "next/link";
 import Blacknavbar from "./Blacknavbar2";
 import Bluenavbar from "./Bluenavbar";
 import Backgroundimage from "./Backgroundimage";
+import Howtoask from "./Howtoask";
+import { FaFacebook, FaTwitter, Fagoogle, FaGofore } from "react-icons/fa";
+
+const object = {
+  data: [
+    {
+      button: "Travel",
+      date: "07 JUNE 2016",
+      title: "How to ask for a haircut you want when you're abroad",
+      subtitle: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Exercitationem impedit libero maiores, itaque atque in sunt eaque consequatur nesciunt dolores hic, ratione excepturi reprehenderit tempora. Saepe nobis debitis incidunt? Maiores!",
+      author: "Post by Author",
+    },
+  ],
+};
 
 function Home() {
   return (
@@ -19,20 +33,23 @@ function Home() {
         <Backgroundimage />
       </section>
 
-
-
-    <section>
-        <div className = "grid grid-cols-2">
-            <div className = "col-span-1">
-                
+      <section>
+        <div className="container mx-auto ">
+          <div className="grid grid-cols-2">
+            <div className="col-span-1">
+              {object.data.map((item, idx) => (
+                <Howtoask
+                  key={idx}
+                  button={item.button}
+                  title={item.title}
+                  subtitle={item.subtitle}
+                  author={item.author}
+                />
+              ))}
             </div>
+          </div>
         </div>
-    </section>
-
-
-
-
-
+      </section>
     </div>
   );
 }
