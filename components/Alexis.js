663cee17078bfa3b5481e80b5b05feca190adf6f
rony <rriachi@bright-lab.com>
2021-09-10 11:55:52 +0300

    work in progress

---
+++ components/Alexis.js
@@ -0,0 +1,24 @@
+import React from 'react'
+
+function Alexis(props) {
+    return (
+        <div className=" border-b-2 container mx-auto">
+        <div className="container mx-auto">
+          <div className="grid grid-cols-2">
+            <div className="col-span-1">
+              <div className = "">
+              <div className="alexisran rounded-full"></div>
+              </div>
+            </div>
+  
+            <div>
+              <div className="font-bold pt-24">{props.name3}</div>
+              <div className="text-gray-500 pt-5 text-sm">{props.position3}</div>
+            </div>
+          </div>
+        </div>
+      </div>
+    )
+}
+
+export default Alexis
