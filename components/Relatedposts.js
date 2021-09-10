b6c0941aa6b04625394ce2f067415715904e6292
rony <rriachi@bright-lab.com>
2021-09-10 11:58:32 +0300

    box absolute text

--- components/Relatedposts.js
+++ components/Relatedposts.js
@@ -9,10 +9,12 @@ function Relatedposts(props) {
               <div className="absolute -bottom-3 -right-3">
                 
                 <div className="bg-white w-60 p-2 h-32 font-bold">
-                  <div className = "flex justify-end items-center">
+                  <div className = "flex justify-end items-center pt-2">
                     <button className = "bg-blue-500 p-1 text-white ">{props.music}{props.musictwo}</button>
                   </div>
+                  <div className = "pt-2">
                   {props.something}{props.somethingtwo}
+                  </div>
                 </div>
               </div>
             </div>
