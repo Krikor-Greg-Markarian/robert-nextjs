c6731f154ae67d96edeb3daf72792e68df37b03f
rony <rriachi@bright-lab.com>
2021-09-10 16:41:24 +0300

    blue navbar

--- components/Bluenavbar.js
+++ components/Bluenavbar.js
@@ -1,35 +1,12 @@
 import React from "react";
 import { FaSearch, FaCommentDots } from "react-icons/fa";
+import Link from 'next/link'
+
 
 function Bluenavbar() {
   return (
     <div>
-      {/* <div className = "bg-blue-500 ">
-          <div className = "container mx-auto">
-              <div className = "">
-                <div className = "flex justify-start items-center">
-                <p className = "italic font-bold text-white inline">Robert</p>
-                </div>
-                <ul className = "inline-block flex justify-center items-center">
-                  <li className = "text-white inline ml-3"><a href="#">Home</a></li>
-                  <li className = "text-white ml-3 inline"><a href="#">About me</a></li>
-                  <li className = "text-white ml-3 inline"><a href="#">Skills</a></li>
-                  <li className = "text-white ml-3 inline"><a href="#">Resume</a></li>
-                  <li className = "text-white ml-3 inline"><a href="#">Service</a></li>
-                  <li className = "text-white ml-3 inline"><a href="#">Portfolio</a></li>
-                  <li className = "text-white ml-3 inline"><a href="#">Team</a></li>
-                  <li className = "text-white ml-3 inline"><a href="#">Hire me</a></li>
-                  
-                <div className = " ">
-                <FaSearch className = "inline text-white "/>
-                <FaCommentDots className = "inline text-white"/>
-                </div>
-                
-                </ul>
-               
-              </div>
-          </div>
-      </div> */}
+     
       <div className="bg-blue-500 p-4 ">
         <div className="container mx-auto ">
           <div className="flex justify-start items-center">
@@ -38,7 +15,7 @@ function Bluenavbar() {
             <div className="pl-8">
               <ul>
                 <li className="text-white inline ml-12">
-                  <a href="#">Home</a>
+                 <Link href="Home"><a>Home</a></Link>
                 </li>
                 <li className="text-white inline ml-5">
                   <a href="#">About me</a>
