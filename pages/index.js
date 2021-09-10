a7f47fa9c2f0ca2e04dad0403d4b5b196198fc3b
rony <rriachi@bright-lab.com>
2021-09-10 15:38:53 +0300

    update

--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,7 @@ import Bluenavbar from "../components/Bluenavbar";
 import Backgroundimage from "../components/Backgroundimage";
 import Smallbox from "../components/Smallbox";
 import Textleftpic from "../components/Textleftpic";
+import styles from "../styles/Home.module.css"
 import {
   FaFacebook,
   FaInstagram,
@@ -11,11 +12,10 @@ import {
   FaMapMarker,
   FaLinkedin,
 } from "react-icons/fa";
-import Alexisran from "../components/Alexisran";
-import Alexisran3 from "../components/Alexisran";
 import Relatedposts from "../components/Relatedposts";
-import Alexis from "../components/Alexis";
 import Footer from "../components/Footer";
+import Shrinkgrow from "../components/Shrinkgrow";
+import Shrinkgrowtwo from "../components/Shrinkgrowtwo";
 
 const object = {
   data: [
@@ -30,6 +30,10 @@ const object = {
       name3: "Alexis Ran",
       position3:
         "Proin gravida nibh vel velit auctor aliquet Aenean sollicitudin. Lorem quisma bibendum ctor, nisi. elitas consequat ipsum nec sagittis sem nibh id elit. Duis sed odios sit amet nibh vulputate cursus a sit amet mauris. Morbi accumsan ipsum velit. Nam nec tellus a odio ticidut.",
+
+        name4: "Alexis Ran",
+      position4:
+        "Integer molestie sapien luctus ex vivera, sit amet vehicula felis pretium. Nunc ac velit curses, sed semper leo scelerisque. Vestibulum tincidunt eutismod arcu venenatis gravida.",
 
       music: "music",
       something: "Tips for Developing a High-end Mobile App that",
@@ -64,8 +68,7 @@ export default function Home() {
         ))}
 
         <div className="container mx-auto border-2 p-4 ">
-          
-          <div className = "">
+          <div className="">
             <p className="font-bold inline border-r-2 pl-4 pr-4">2 COMMENTS </p>
             <p className="font-bold inline ml-1 pl-4 pr-4 "> TAGGED :</p>
             <span className="text-gray-500 inline">LIFESTYLE, FOOD, DRINK</span>
@@ -82,7 +85,7 @@ export default function Home() {
 
       <section>
         {object.data.map((item, idx) => (
-          <Alexisran key={idx} name={item.name} position={item.position} />
+          <Shrinkgrow key={idx} name={item.name} position={item.position} />
         ))}
       </section>
 
@@ -102,89 +105,54 @@ export default function Home() {
                     something={item.something}
                   />
                 ))}
-                
               </div>
               {object.data.map((item, idx) => (
-                  <Relatedposts
-                    key={idx}
-                    musictwo={item.musictwo}
-                    somethingtwo={item.somethingtwo}
-                  />
-                ))}
+                <Relatedposts
+                  key={idx}
+                  musictwo={item.musictwo}
+                  somethingtwo={item.somethingtwo}
+                />
+              ))}
             </div>
 
             <div className="flex justify-center items-center pt-20">
               <p className="font-bold border-b-4 text-sm">COMMENTS</p>
             </div>
 
+            <div className="border-b-2">
+              {object.data.map((item, idx) => (
+                <Shrinkgrowtwo
+                  key={idx}
+                  name3={item.name3}
+                  position3={item.position3}
+                />
+                
+              ))}
+              <div className ="pt-20 "></div>
+            </div>
             <div className="">
-              <div className="pt-20 border-b-2  container mx-auto pb-20">
-                <div className="container mx-auto p-20">
-                  <div className="grid grid-cols-2 ">
-                    <div className="col-span-1">
-                      <div className=" p-20 ">
-                        <div className="alexisran rounded-full "></div>
-                      </div>
-                    </div>
-
-                    <div className="">
-                      <div className="font-bold inline">
-                        {" "}
-                        Alexis Ran{" "}
-                        <p className="text-gray-500 text-sm pl-4 inline">
-                          March 15, 2016{" "}
-                        </p>{" "}
-                        <span className="text-gray-500 text-sm float-right pt-1">
-                          REPLY
-                        </span>{" "}
-
-                      </div>
-                      <div className="text-gray-500 pt-5 text-sm">
-                        Proin gravida nibh vel velit auctor aliquet Aenean
-                        sollicitudin. Lorem quisma bibendum ctor, nisi. elitas
-                        consequat ipsum nec sagittis sem nibh id elit. Duis sed
-                        odios sit amet nibh vulputate cursus a sit amet mauris.
-                        Morbi accumsan ipsum velit. Nam nec tellus a odio
-                        ticidut.
-                      </div>
-                    </div>
-
-                    <div className="col-span-1 pt-12 border-t-2">
-                      <div className="pt-32 pl-20 ">
-                        <div className="alexisran rounded-full"></div>
-                      </div>
-                    </div>
-                    <div className="pt-36 border-t-2">
-                      <div className="font-bold inline">
-                        {" "}
-                        Alexis Ran{" "}
-                        <p className="text-gray-500 text-sm pl-4 inline">
-                          March 15, 2016{" "}
-                        </p>{" "}
-                        <span className="text-gray-500 text-sm float-right pt-1">
-                          REPLY
-                        </span>{" "}
-                      </div>
-                      <div className="text-gray-500 pt-5 text-sm">
-                        Proin gravida nibh vel velit auctor aliquet Aenean
-                        sollicitudin. Lorem quisma bibendum ctor, nisi. elitas
-                        consequat ipsum nec sagittis sem nibh id elit. Duis sed
-                        odios sit amet nibh vulputate cursus a sit amet mauris.
-                        Morbi accumsan ipsum velit. Nam nec tellus a odio
-                        ticidut.
-                      </div>
-                    </div>
-                  </div>
-                </div>
-              </div>
+              {object.data.map((item, idx) => (
+                <Shrinkgrowtwo
+                  key={idx}
+                  name4={item.name4}
+                  position4={item.position4}
+                />
+                
+              ))}
+              
             </div>
           </div>
         </div>
       </section>
 
+
+
+
       <section>
         <Footer />
       </section>
+
+      <section className="container mx-auto"></section>
     </div>
   );
 }
