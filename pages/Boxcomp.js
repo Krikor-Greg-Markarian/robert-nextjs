7b4b9a8ccc2d726bd1d2e835411d39cffeba980f
rony <rriachi@bright-lab.com>
2021-09-13 13:48:55 +0300

    small box components

---
+++ pages/Boxcomp.js
@@ -0,0 +1,21 @@
+import React from 'react'
+
+function Boxcomp() {
+    return (
+        <div className = "pl-24">
+            <div className = "container mx-auto bg-white p-3">
+                <div className = "flex justify-center items-center">
+                    <p className = "text-gray">----  <span className = "text-black font-bold text-sm">ABOUT ME</span>  ----</p>
+                   
+                </div>
+                <div className = "flex justify-center items-center pt-4">
+                <div className = "boywitheyeglass"></div>
+                </div>
+                <p className = "text-gray-500 text-center p-2 text-sm">A personal diary of wanderlust and an overflowing wardrobe. LIve with passion.</p>
+                <p className = "font-bold italic pt-1 text-center pb-6">Robert Smith</p>
+            </div>
+        </div>
+    )
+}
+
+export default Boxcomp
