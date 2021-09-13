b0ac14311c06fc019596a013eb60487ee17be36d
rony <rriachi@bright-lab.com>
2021-09-13 16:58:39 +0300

    component date title text author

--- pages/Howtoask.js
+++ pages/Howtoask.js
@@ -3,9 +3,21 @@ import { FaFacebook, FaTwitter, FaGooglePlusG,FaFacebookMessenger,  } from 'reac
 import { IoIosArrowRoundForward } from "react-icons/io";
 
 function Howtoask(props) {
+    const imgUrl1 = props.hikingimage3;
+    const imgUrl2 = props.hikingimage4;
+    const imgUrl3 = props.hikingimage5;
+    const imgUrl4 = props.hikingimage6;
+    
+    
+    
     return (
+
+        
         <div className = "bg-white">
-            <div className = "hikingimage3 rounded-t-lg p-6">
+
+            <div style = {{backgroundImage: `url("${imgUrl1}")`}}  
+
+            className = "hikingimage3 rounded-t-lg p-6">
                 <button className = "bg-blue-500 p-2 text-white rounded-lg">{props.button}{props.button2}{props.button3}{props.button4}</button>
             </div>
             <div className = "p-6">
